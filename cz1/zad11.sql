5a711bd2c6c4bfd9f55ee3c59bb96445da6730ec
Kijanczuk, Damian <kijaczukdamian@gmail.com>
2021-06-12 19:23:11 +0200

    Fix zad11

--- cz1/zad11.sql
+++ cz1/zad11.sql
@@ -1,6 +1,7 @@
-INSERT INTO Employees (Employee_ID, Firstname,Lastname,Job_ID,Salary,Manager_ID, Department_ID) VALUES (510, "Mateusz", "Miotk", "UG", 1000.00, 201, 60);
-INSERT INTO Employees VALUES (511, "Jan", "Kowalski", "PG", 1500.50, 351, 62);
-INSERT INTO Employees VALUES (512, "Anna", "Nowak", "WSB", 1234.00, 451, 63);
 INSERT INTO Departments VALUES (60, "Informatyka", 201, 100);
 INSERT INTO Departments VALUES (62, "Matematyka", 351, 200);
 INSERT INTO Departments VALUES (63, "Ekonomia", 451, 300);
+INSERT INTO Employees VALUES (510, "Mateusz", "Miotk", "UG", 1000.00, 201, 60);
+INSERT INTO Employees VALUES (511, "Jan", "Kowalski", "PG", 1500.50, 351, 62);
+INSERT INTO Employees VALUES (512, "Anna", "Nowak", "WSB", 1234.00, 451, 63);
+
